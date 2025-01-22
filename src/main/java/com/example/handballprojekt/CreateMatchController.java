ffe1436a1505f008879f737b67094e5ffd49e31c
Arni Gaardbo <gaardbo2605@gmail.com>
2025-01-22 22:32:49 +0100

    Match

--- src/main/java/com/example/handballprojekt/CreateMatchController.java
+++ src/main/java/com/example/handballprojekt/CreateMatchController.java
@@ -1,4 +1,34 @@
 package com.example.handballprojekt;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class CreateMatchController {
+
+    @FXML
+    private Button goBackToMatchView;
+
+    @FXML
+    void onAction_GoToMatchView(ActionEvent event) {
+        try {
+
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("MatchView.fxml"));
+            Parent root = loader.load();
+
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            stage.setScene(new Scene(root));
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
